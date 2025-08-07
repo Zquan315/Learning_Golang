87eac0a0f04d8a738bd0ac5dd5373f62fe537f66
Zquan3101 <22521190@gm.uit.edu.vn>
2025-08-07 22:07:40 +0700

    week5-day4: Learning methods in REST API - PUT and DELETE

--- week5-basic-REST-API/day2-3-4-method/main.go
+++ week5-basic-REST-API/day2-3-4-method/main.go
@@ -25,19 +25,23 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewEncoder(w).Encode(users)
 	if err != nil {
-		http.Error(w, "Error in dencoding users", http.StatusInternalServerError)
+		http.Error(w, "❌ Error in dencoding users", http.StatusInternalServerError)
 	}
 
 }
 func GetUserWithID(w http.ResponseWriter, r *http.Request) {
 	id := strings.TrimPrefix(r.URL.Path, "/user/")
+	if id == "" {
+		http.Error(w, "ID is empty!", http.StatusInternalServerError)
+		return
+	}
 	for _, v := range users {
 		if id == v.ID {
 			w.Header().Set("Content-Type", "application/json")
 
 			err := json.NewEncoder(w).Encode(v)
 			if err != nil {
-				http.Error(w, "Error in dencoding users", http.StatusInternalServerError)
+				http.Error(w, "❌ Error in dencoding users", http.StatusInternalServerError)
 			}
 			break
 		}
@@ -54,12 +58,67 @@ func PostUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		f.Fprint(w, "Error in decode json to User")
+		f.Fprint(w, "❌ Error in decode json to User")
 		return
 	}
 
 	users = append(users, user)
-	f.Fprint(w, "Add successfully a user with ID:", user.ID, " and Name: ", user.Name)
+	f.Fprint(w, "✅ Add successfully a user with ID:", user.ID, " and Name: ", user.Name)
+}
+
+func PutUserWithID(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "This is Put method.", http.StatusInternalServerError)
+		return
+	}
+	id := strings.TrimPrefix(r.URL.Path, "/user/update/")
+	if id == "" {
+		http.Error(w, "❌ ID is empty!", http.StatusInternalServerError)
+		return
+	}
+	flag := false
+	for i, v := range users {
+		if id == v.ID {
+			w.Header().Set("Content-Type", "application/json")
+
+			err := json.NewDecoder(r.Body).Decode(&users[i])
+			if err != nil {
+				http.Error(w, "Error in dencoding user", http.StatusInternalServerError)
+			}
+			flag = true
+			f.Fprintf(w, "✅ Update user with ID: %s and Name: %s successfully", users[i].ID, users[i].Name)
+			break
+		}
+	}
+	if !flag {
+		f.Fprintf(w, "❌ %s is not exists", id)
+	}
+}
+
+func DeleteUserWithID(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		http.Error(w, "❌ This is Delete method.", http.StatusInternalServerError)
+		return
+	}
+	id := strings.TrimPrefix(r.URL.Path, "/user/del/")
+	if id == "" {
+		http.Error(w, "❌ ID is empty!", http.StatusInternalServerError)
+		return
+	}
+	flag := false
+	for i, v := range users {
+		if id == v.ID {
+			w.Header().Set("Content-Type", "application/json")
+
+			users = append(users[:i], users[i+1:]...)
+			flag = true
+			f.Fprintf(w, "✅ Delete user with ID: %s successfully", id)
+			break
+		}
+	}
+	if !flag {
+		f.Fprintf(w, "❌ %s is not exists", id)
+	}
 }
 
 func Init() {
@@ -77,6 +136,8 @@ func Handler() {
 	http.HandleFunc("/", HomePage)
 	http.HandleFunc("/users", GetUsers)
 	http.HandleFunc("/add-user", PostUser)
+	http.HandleFunc("/user/del/", DeleteUserWithID)
+	http.HandleFunc("/user/update/", PutUserWithID)
 	http.HandleFunc("/user/", GetUserWithID)
 	f.Println("Server is listening on: http://localhost:3101")
 	http.ListenAndServe(":3101", nil)
