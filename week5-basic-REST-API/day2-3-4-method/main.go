bce5860f7728121ce9d02d0801e3774a08ac6f4d
Zquan3101 <22521190@gm.uit.edu.vn>
2025-08-07 21:26:32 +0700

    week5-day3: Learning methods in REST API - POST

--- week5-basic-REST-API/day2-3-4-method/main.go
+++ week5-basic-REST-API/day2-3-4-method/main.go
@@ -9,7 +9,7 @@ import (
 
 )
 type User struct {
-	ID string `json:"Id"`
+	ID string `json:"ID"`
 	Name string `json:"Name"`
 }
 
@@ -44,6 +44,24 @@ func GetUserWithID(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func PostUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "This is method Post.", http.StatusInternalServerError)
+		return
+	}
+
+	var user User
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		f.Fprint(w, "Error in decode json to User")
+		return
+	}
+
+	users = append(users, user)
+	f.Fprint(w, "Add successfully a user with ID:", user.ID, " and Name: ", user.Name)
+}
+
 func Init() {
 	for i:=0; i < 3 ; i++ {
 		s := strconv.Itoa(i)
@@ -58,6 +76,7 @@ func Init() {
 func Handler() {
 	http.HandleFunc("/", HomePage)
 	http.HandleFunc("/users", GetUsers)
+	http.HandleFunc("/add-user", PostUser)
 	http.HandleFunc("/user/", GetUserWithID)
 	f.Println("Server is listening on: http://localhost:3101")
 	http.ListenAndServe(":3101", nil)
